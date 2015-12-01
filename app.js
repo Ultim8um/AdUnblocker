6282447f1bc7261981b65a94682977a2f22f80be
Eduardo Aceituno <edaceituno@gmail.com>
2015-11-30 17:21:06 -0800

    Implemented random directory and image Added tmp module for randomizing names Updated jade index top reflect new features/changes Reorganized code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// Acquire Express object
+/* ------------Dependencies------------ */
 var express =  require('express');
-var app = express();
-
-// Acquire file system module
 var fs = require('fs');
-// Acquire path module
 var path = require('path');
+var tmp = require('tmp');
+
+// Initialize express app
+var app = express();
 
 // Set view folder
 app.set('views', './views');
@@ -16,19 +16,39 @@ app.set('view engine', 'jade');
 // Set assets folder
 app.use(express.static('public'));
 
-// Set Home route
-app.get('/', function(req, res) {
+// Home route
+app.get('/', function(req, res) {    
     fs.readFile(path.join(__dirname, 'public/images/ad.jpg'), function (err, data) {
         if (err) throw err;
         
-        // TODO: Randomized file and directory
+        // Generate random image file
+        var randImgName = tmp.tmpNameSync({mode: 0644, prefix: '_', postfix: '.jpg', dir: './public/images/'});
+        // Generate random directory
+        var randDirName = tmp.dirSync({template: './public/XXXXXX'});
         
-        // Respond with jade index
+        // Create random image file
+        fs.writeFileSync(randImgName, data);
+        console.log("Successfully created random image at: ", randImgName);
+        
+        // Extract image from randImgName path
+        var randImg = path.parse(randImgName);
+        // Construct random directory name + image
+        var randDir = path.basename(randDirName.name) + '/' + randImg.base;
+        
+        // Create random directory + image
+        fs.writeFileSync(path.join(__dirname, 'public/' + randDir), data);
+        console.log("Successfully created random directory and image at: ", randDir);       
+        
+        // Respond with jade index template and context
         res.render('index', {
-            src: data.toString('base64')
+            encoded_img: data.toString('base64'),
+            random_img: randImg.base,
+            random_dir: randDir
         });
     });    
 });
 
 // Listen on http://localhost:3000
-var server = app.listen(3000);
+var server = app.listen(3000, function() {
+    console.log('Server listening on http://localhost:3000');
+});
